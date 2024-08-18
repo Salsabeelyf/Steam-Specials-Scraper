5f2280fa364440023cee8f2ea386c43dde46c9c6
Salsabeelyf <salsabeelsay@gmail.com>
2024-08-18 16:53:12 +0100

    Add timestamp to output file name

--- helpers/output.py
+++ helpers/output.py
@@ -1,8 +1,10 @@
 # Export info into output csv file
 import pandas as pd
+from datetime import datetime
 
 # export games info to csv file
 def export_to_csv(games: list, output_file):
     df = pd.DataFrame(games)
-    print(f'Exporting to {output_file} .....')
-    df.to_csv(output_file, index=False)
+    file_name = f'{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}_{output_file}'
+    print(f'Exporting to {file_name} .....')
+    df.to_csv(f'{file_name}', index=False)
